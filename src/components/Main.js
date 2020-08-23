7e1af06c1c6145af2862d33e7d4e6f785db55b47
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-08-23 15:40:38 +0800

    game of life logic implemented into code, board now runs the simulation but no pause functionality yet

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,6 @@ class Main extends React.Component {
 
     this.state = {
       generation: 0,
-      // for when the grid is full
       gridFull: Array(this.rows)
         .fill()
         .map(() => Array(this.cols).fill(false))
@@ -39,8 +38,48 @@ class Main extends React.Component {
     });
   };
 
+  playButton = () => {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(this.play, this.speed);
+  };
+
+  pauseButton = () => {
+    clearInterval(this.intervalId);
+  };
+
+  play = () => {
+    let g = this.state.gridFull;
+    let g2 = arrayClone(this.state.gridFull);
+
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let count = 0; // how many neighbors the cell have
+
+        // below 8 LOC checks for neighbors, which updates count accordingly
+        if (i > 0) if (g[i - 1][j]) count++;
+        if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+        if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+        if (j < this.cols - 1) if (g[i][j + 1]) count++;
+        if (j > 0) if (g[i][j - 1]) count++;
+        if (i < this.rows - 1) if (g[i + 1][j]) count++;
+        if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+        if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+
+        // below 2 LOC updates status of cell: alive or dead
+        if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false; // if <2 or >3 neighbors, the cell dies
+        if (!g[i][j] && count == 3) g2[i][j] = true; // if dead cell has 3 neighbors, the cell lives
+      }
+    }
+
+    this.setState({
+      gridFull: g2,
+      generation: this.state.generation + 1
+    });
+  };
+
   componentDidMount() {
     this.seed();
+    this.playButton();
   }
 
   render() {

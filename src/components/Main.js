43e5213c6edd6c88def7da395126c8fea8d4e59f
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-08-26 11:24:26 +0800

    built presets, might refactor once first-pass solution is complete

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Grid from "./Grid";
 import Buttons from "./Buttons";
+import Presets from "./Presets";
 
 class Main extends React.Component {
   constructor() {
@@ -95,7 +96,7 @@ class Main extends React.Component {
 
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
-        let count = 0; // how many neighbors the cell have
+        let count = 0; // how many neighbors each cell has
 
         // below 8 LOC checks for neighbors, which updates count accordingly
         if (i > 0) if (g[i - 1][j]) count++;
@@ -119,6 +120,159 @@ class Main extends React.Component {
     });
   };
 
+  // PRESETS
+  glider = () => {
+    this.clear();
+    let gridCopy = arrayClone(this.state.gridFull);
+
+    gridCopy[1][3] = true;
+    gridCopy[2][1] = true;
+    gridCopy[2][3] = true;
+    gridCopy[3][2] = true;
+    gridCopy[3][3] = true;
+
+    this.setState({
+      gridFull: gridCopy
+    });
+  };
+
+  pentaDecathlon = () => {
+    this.clear();
+    let gridCopy = arrayClone(this.state.gridFull);
+
+    gridCopy[14][20] = true;
+    gridCopy[14][21] = true;
+    gridCopy[14][23] = true;
+    gridCopy[14][24] = true;
+    gridCopy[14][25] = true;
+    gridCopy[14][26] = true;
+    gridCopy[14][28] = true;
+    gridCopy[14][29] = true;
+    gridCopy[13][22] = true;
+    gridCopy[15][22] = true;
+    gridCopy[13][27] = true;
+    gridCopy[15][27] = true;
+
+    this.setState({
+      gridFull: gridCopy
+    });
+  };
+
+  pulsar = () => {
+    let gridCopy = arrayClone(this.state.gridFull);
+
+    // gridCopy[6][18]
+    gridCopy[6][20] = true;
+    gridCopy[6][21] = true;
+    gridCopy[6][22] = true;
+    gridCopy[6][26] = true;
+    gridCopy[6][27] = true;
+    gridCopy[6][28] = true;
+
+    gridCopy[8][18] = true;
+    gridCopy[8][23] = true;
+    gridCopy[8][25] = true;
+    gridCopy[8][30] = true;
+
+    gridCopy[9][18] = true;
+    gridCopy[9][23] = true;
+    gridCopy[9][25] = true;
+    gridCopy[9][30] = true;
+
+    gridCopy[10][18] = true;
+    gridCopy[10][23] = true;
+    gridCopy[10][25] = true;
+    gridCopy[10][30] = true;
+
+    gridCopy[11][20] = true;
+    gridCopy[11][21] = true;
+    gridCopy[11][22] = true;
+    gridCopy[11][26] = true;
+    gridCopy[11][27] = true;
+    gridCopy[11][28] = true;
+
+    gridCopy[13][20] = true;
+    gridCopy[13][21] = true;
+    gridCopy[13][22] = true;
+    gridCopy[13][26] = true;
+    gridCopy[13][27] = true;
+    gridCopy[13][28] = true;
+
+    gridCopy[14][18] = true;
+    gridCopy[14][23] = true;
+    gridCopy[14][25] = true;
+    gridCopy[14][30] = true;
+
+    gridCopy[15][18] = true;
+    gridCopy[15][23] = true;
+    gridCopy[15][25] = true;
+    gridCopy[15][30] = true;
+
+    gridCopy[16][18] = true;
+    gridCopy[16][23] = true;
+    gridCopy[16][25] = true;
+    gridCopy[16][30] = true;
+
+    gridCopy[18][20] = true;
+    gridCopy[18][21] = true;
+    gridCopy[18][22] = true;
+    gridCopy[18][26] = true;
+    gridCopy[18][27] = true;
+    gridCopy[18][28] = true;
+
+    this.setState({
+      gridFull: gridCopy
+    });
+  };
+
+  gosperGun = () => {
+    let gridCopy = arrayClone(this.state.gridFull);
+
+    gridCopy[6][1] = true;
+    gridCopy[6][2] = true;
+    gridCopy[7][1] = true;
+    gridCopy[7][2] = true;
+
+    gridCopy[6][11] = true;
+    gridCopy[7][11] = true;
+    gridCopy[8][11] = true;
+    gridCopy[5][12] = true;
+    gridCopy[9][12] = true;
+    gridCopy[4][13] = true;
+    gridCopy[4][14] = true;
+    gridCopy[10][13] = true;
+    gridCopy[10][14] = true;
+    gridCopy[7][15] = true;
+    gridCopy[5][16] = true;
+    gridCopy[9][16] = true;
+    gridCopy[6][17] = true;
+    gridCopy[8][17] = true;
+    gridCopy[7][17] = true;
+    gridCopy[7][18] = true;
+
+    gridCopy[4][21] = true;
+    gridCopy[5][21] = true;
+    gridCopy[6][21] = true;
+    gridCopy[4][22] = true;
+    gridCopy[5][22] = true;
+    gridCopy[6][22] = true;
+    gridCopy[3][23] = true;
+    gridCopy[7][23] = true;
+    gridCopy[2][25] = true;
+    gridCopy[3][25] = true;
+    gridCopy[7][25] = true;
+    gridCopy[8][25] = true;
+
+    gridCopy[4][35] = true;
+    gridCopy[4][36] = true;
+    gridCopy[5][35] = true;
+    gridCopy[5][36] = true;
+
+    this.setState({
+      gridFull: gridCopy
+    });
+  };
+
   render() {
     return (
       <div>
@@ -131,6 +285,12 @@ class Main extends React.Component {
           clear={this.clear}
           seed={this.seed}
           gridSize={this.gridSize}
+        />
+        <Presets
+          glider={this.glider}
+          pentaDecathlon={this.pentaDecathlon}
+          pulsar={this.pulsar}
+          gosperGun={this.gosperGun}
         />
         <Grid
           gridFull={this.state.gridFull}
